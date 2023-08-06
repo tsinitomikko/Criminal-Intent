d1d469626d99bec7f0f7ab33acd1ba34234d8cc2
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-06 13:56:07 -0500

    feat: chapter 10 - displaying list with recyclerview
    Signed-off-by: Mikko <18387920+tsinitomikko@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
     testImplementation("junit:junit:4.13.2")
+    debugImplementation("androidx.test:monitor:1.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
