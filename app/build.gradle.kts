22735695400b34a16672e6851d129c95e9cc0bb9
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-07 15:02:16 -0500

    feat: chapter 12 - (challenge)
    Signed-off-by: Mikko <18387920+tsinitomikko@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,14 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += mapOf(
+                    "room.schemaLocation" to "$projectDir/schemas",
+                )
+            }
+        }
     }
 
     buildTypes {
