2621463fdfa571997bbe322125761deefc57a9d2
Mikko <18387920+tsinitomikko@users.noreply.github.com>
2023-08-07 00:48:26 -0500

    feat: chapter 11 - creating user interfaces with layouts and views
    Signed-off-by: Mikko <18387920+tsinitomikko@users.noreply.github.com>

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +18,12 @@ class CrimeHolder(
             Toast.makeText(
                 binding.root.context, "${crime.title} clicked!", Toast.LENGTH_SHORT
             ).show()
+        }
+
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
         }
     }
 }
